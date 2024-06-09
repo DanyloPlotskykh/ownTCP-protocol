49dd74acf1e3de08a2e78c7ec359678ede551fc9
DanyloPlotskykh <145497398+DanyloPlotskykh@users.noreply.github.com>
2024-06-09 14:37:33 +0300

    Implement syn (#4)
    * packet with tcp header
    
    * 1 step - SYN + create packet func

--- _client/Sender.cpp
+++ _client/Sender.cpp
@@ -1,5 +1,28 @@
 #include "Sender.hpp"
 #include <cstring>
+tcp_hdr& tcp_hdr::operator=(const tcp_hdr& other)
+{
+    if (this == &other) {
+        return *this;
+    }
+    std::cout << "tcp_hdr::operator=()" << std::endl;
+    this->number = other.number;
+    this->ack_number = other.ack_number;
+    this->len = other.len;
+    this->reserved = other.reserved;
+    this->ns = other.ns;
+    this->cwr = other.cwr;
+    this->ece = other.ece;
+    this->fin = other.fin;
+    this->syn = other.syn;
+    this->rst = other.rst;
+    this->psh = other.psh;
+    this->ack = other.ack;
+    this->urg = other.urg;
+    this->window_size = other.window_size;
+    this->SACK = other.SACK;
+    return *this;
+}
 
 unsigned short calculate_checksum(void* b, size_t len)
 {
@@ -44,6 +67,35 @@ Sender::~Sender()
 //     return m_buffer;
 // }
 
+int temp;
+
+std::array<char, 1024> Sender::create_packet(const struct tcp_hdr& tcp, const char* data, int data_size)
+{
+    std::array<char, 1024> packet;
+    struct iphdr *iph = (struct iphdr *)packet.data();
+    iph->ihl = 5;
+    iph->version = 4;
+    iph->tos = 0;
+    iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct tcp_hdr) + data_size);
+    iph->id = htonl(54321);
+    iph->frag_off = 0;
+    iph->ttl = 255;
+    iph->protocol = IPPROTO_UDP;
+    iph->check = calculate_checksum(packet.data(), sizeof(struct iphdr));
+    iph->saddr = inet_addr(m_addr.c_str());
+    iph->daddr = m_servaddr.sin_addr.s_addr;
+
+    struct udphdr *udph = (struct udphdr *)(packet.data() + sizeof(struct iphdr));
+    udph->source = htons(12345);
+    udph->dest = htons(m_port);
+    udph->len = htons(sizeof(struct udphdr) + sizeof(struct tcp_hdr));
+    udph->check = htons(calculate_checksum(packet.data(), sizeof(struct udphdr)));
+    temp = iph->tot_len;
+    struct tcp_hdr *tcph = (struct tcp_hdr *)(packet.data() + sizeof(struct iphdr) + sizeof(struct udphdr));
+    *tcph = tcp;
+    memcpy(packet.data() + sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct tcp_hdr), data, data_size);
+    return packet;
+}
 bool Sender::connect()
 {
     //create packet
@@ -52,33 +104,35 @@ bool Sender::connect()
     //send ack
     const char *message = "Hello, Server!";
     int message_len = strlen(message);
-    struct iphdr *iph = (struct iphdr *)m_buffer;
-    iph->ihl = 5;
-    iph->version = 4;
-    iph->tos = 0;
-    iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct tcp_hdr) + message_len);
-    iph->id = htonl(54321);
-    iph->frag_off = 0;
-    iph->ttl = 255;
-    iph->protocol = IPPROTO_UDP;
-    iph->check = calculate_checksum(m_buffer, sizeof(struct iphdr));
-    iph->saddr = inet_addr(m_addr.c_str());
-    iph->daddr = m_servaddr.sin_addr.s_addr;
+    // struct iphdr *iph = (struct iphdr *)m_buffer;
+    // iph->ihl = 5;
+    // iph->version = 4;
+    // iph->tos = 0;
+    // iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct tcp_hdr) + message_len);
+    // iph->id = htonl(54321);
+    // iph->frag_off = 0;
+    // iph->ttl = 255;
+    // iph->protocol = IPPROTO_UDP;
+    // iph->check = calculate_checksum(m_buffer, sizeof(struct iphdr));
+    // iph->saddr = inet_addr(m_addr.c_str());
+    // iph->daddr = m_servaddr.sin_addr.s_addr;
 
-    struct udphdr *udph = (struct udphdr *) (m_buffer + sizeof(struct iphdr));
-    udph->source = htons(12345);
-    udph->dest = htons(m_port);
-    udph->len = htons(sizeof(struct udphdr) + sizeof(struct tcp_hdr));
-    udph->check = calculate_checksum(&m_buffer, sizeof(struct udphdr));
+    // struct udphdr *udph = (struct udphdr *) (m_buffer + sizeof(struct iphdr));
+    // udph->source = htons(12345);
+    // udph->dest = htons(m_port);
+    // udph->len = htons(sizeof(struct udphdr) + sizeof(struct tcp_hdr));
+    // udph->check = htons(calculate_checksum(&m_buffer, sizeof(struct udphdr)));
 
-    tcp_hdr *tcp = (struct tcp_hdr *) (m_buffer + sizeof(struct iphdr) + sizeof(struct udphdr));
-    tcp->number = htons(34);
-    tcp->ack_number = htons(47);
-    tcp->len = htons(sizeof(struct tcp_hdr));
-    tcp->syn = htons(1);
+    tcp_hdr tcp;
+    tcp.number = htons(34);
+    tcp.ack_number = htons(47);
+    tcp.len = 7/*calculate_checksum(m_buffer, sizeof(struct tcp_hdr))*/;
+    std::cout << "calc - " << tcp.len << std::endl;
+    tcp.syn = 1;
 
-    memcpy(m_buffer + sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct tcp_hdr), message, message_len);
-    if (sendto(m_sockfd, m_buffer, ntohs(iph->tot_len), 0, (struct sockaddr *)&m_servaddr, sizeof(m_servaddr)) < 0) {
+    // memcpy(m_buffer + sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct tcp_hdr), message, message_len);
+    auto packet = create_packet(tcp, message, message_len);
+    if (sendto(m_sockfd, packet.data(), ntohs(temp), 0, (struct sockaddr *)&m_servaddr, sizeof(m_servaddr)) < 0) {
         perror("sendto failed");
     } else {
         std::cout << "Message sent to server." << std::endl;
---
+++ _server/Reciever.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <queue>
+#include <array>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUFFER_SIZE 1024
+
+struct tcp_hdr
+{
+    uint32_t number;
+    uint32_t ack_number;
+    uint16_t len:4, reserved:3, ns:1, cwr:1, ece:1, fin:1, syn:1, rst:1, psh:1, ack:1, urg:1;
+    uint16_t window_size;
+    uint16_t SACK;
+
+    tcp_hdr& operator=(const tcp_hdr& other);
+};
+ 
+struct pars
+{
+    struct iphdr ip;
+    struct udphdr udp;
+    struct tcp_hdr tcp;
+};
+
+class Interface
+{
+private:
+    std::array<char, 1024> m_buffer;
+    pars p;
+public:
+    explicit Interface(const char* packet);
+    ~Interface();
+    iphdr ipHeader() const;
+    udphdr udpHeader() const;
+    tcp_hdr tcpHeader() const;
+    char * data() const;
+};
+
+class Reciever
+{
+private:
+    std::queue<Interface> m_window;
+    int ack;
+    int byte;
+    int m_sockfd;
+    struct sockaddr_in m_servaddr, m_cliaddr;
+    //std::array<char, 1024> create_packet(const struct tcp_hdr& tcp, const char* data, int data_size);
+public:
+    Reciever();
+    ~Reciever();
+    int accept();
+};
